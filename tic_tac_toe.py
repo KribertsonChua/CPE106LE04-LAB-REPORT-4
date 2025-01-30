00507001da7c8090301e3c16f37b169dbb449d11
AndreaQuiambao <156178864+AndreaQuiambao@users.noreply.github.com>
2025-01-30 22:57:22 +0800

    Added tic tac toe python code

---
+++ tic_tac_toe.py
@@ -0,0 +1,14 @@
+
+class TicTacToe:
+    def __init__(self):
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.current_player = 'X'
+    
+    def play_move(self, row, col):
+        if self.board[row][col] == ' ':
+            self.board[row][col] = self.current_player
+            self.current_player = 'O' if self.current_player == 'X' else 'X'
+    
+    def get_current_player(self):
+        return self.current_player
+
